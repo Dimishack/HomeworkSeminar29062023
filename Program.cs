b33ce71758b2ca5974f438aa47df82dc43065f7e
Dimishack <dimishack.git@mail.ru>
2023-06-30 12:50:13 +0300

    Добавление обработчика ошибок в Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ namespace HomeworkSeminar29062023
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Нажмите на любую клавишу для выхода из программы...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
